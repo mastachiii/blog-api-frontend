e328e32254c5bdc1e8b8665e116c8e101046e6c2
mastachii <mastachii273@gmail.com>
2025-01-25 17:52:16 +0800

    wip: Editor

--- routes/routes.jsx
+++ routes/routes.jsx
@@ -6,6 +6,7 @@ import Index from "../src/components";
 import LogOut from "../src/components/logOut";
 import BlogPage from "../src/components/blog";
 import Error from "../src/components/error";
+import Editor from "../src/components/editor";
 
 function isAuthenticated({ type }) {
     const token = localStorage.getItem("token");
@@ -46,6 +47,10 @@ const route = createBrowserRouter([
             {
                 path: "/author",
                 element: <Index isAuthor={true} />,
+            },
+            {
+                path: "/author/edit",
+                element: <Editor />,
             },
         ],
     },
---
+++ src/components/editor.jsx
@@ -0,0 +1,7 @@
+export default function Editor() {
+    return (
+        <form action="">
+            <textarea id="editor"></textarea>
+        </form>
+    );
+}
