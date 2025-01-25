11e4ec3353f0ef1538911413d9709ad08961ea95
mastachii <mastachii273@gmail.com>
2025-01-25 20:02:40 +0800

    wip: Creating new posts

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 export default function Editor() {
+    const [title, setTitle] = useState("");
+    const [isPrivate, setIsPrivate] = useState(false);
+    const [bruh, setBruh] = useState(null);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const body = tinymce.get("editor").getContent();
+        setBruh(body);
+    }
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <label htmlFor="title">Title: </label>
-            <input type="text" name="title" id="title" required />
+            <input type="text" value={title} onChange={e => setTitle(e.target.value)} id="title" required />
             <label htmlFor="editor"></label>
-            <textarea id="editor" name="body"></textarea>
+            <textarea id="editor"></textarea>
             <label htmlFor="private">Make private?</label>
-            <input type="checkbox" name="private" id="private" />
-            <button type="button">Post</button>
+            <input type="checkbox" checked={isPrivate} onChange={e => setIsPrivate(e.target.checked)} id="private" />
+            <button type="submit">Post</button>
+            <div dangerouslySetInnerHTML={{ __html: bruh }}></div>
         </form>
     );
 }
