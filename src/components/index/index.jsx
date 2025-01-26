390f53d5dbea134d41af2822a5d94191fa8c3f0e
mastachii <mastachii273@gmail.com>
2025-01-26 16:06:43 +0800

    feat: Finished styling index page

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -3,6 +3,7 @@ import Blog from "../../helpers/blogApi";
 import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "./index.module.css";
+import { format } from "date-fns";
 
 export default function Index({ isAuthor }) {
     const [posts, setPosts] = useState([]);
@@ -26,12 +27,16 @@ export default function Index({ isAuthor }) {
             <div>
                 <div className={styles.imgContainer}></div>
                 <div className={styles.postsContainer}>
+                    <h3>Komorebi</h3>
                     <h3>Posts</h3>
                     <ul>
                         {posts.map(p => {
                             return (
                                 <li key={p.id}>
+                                    <img src={p.backdropUrl} alt="" />
+
                                     <Link to={`/posts/${p.id}`}>{p.title}</Link>
+                                    <p>{format(new Date(p.createdAt), "PPP")}</p>
                                     {isAuthor && (
                                         <div>
                                             <a href={`/author/edit/${p.id}`}>EDIT</a>
